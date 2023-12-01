ba2167a271c4e4a6cb24f37afcef95150102821e
Blake Hildebrand <baugusthildebrand@gmail.com>
2023-12-01 16:58:25 -0500

    Add command to fetch input

---
+++ src/fetch_input.rs
@@ -0,0 +1,26 @@
+use std::env;
+use std::fs::File;
+use std::io::Write;
+
+use reqwest::blocking::Client;
+
+pub fn fetch_input(day: usize) {
+    let cookie = env::var("AOC_COOKIE").expect("AOC_COOKIE not set");
+    let url = format!("https://adventofcode.com/2023/day/{}/input", day);
+
+    let client = Client::new();
+    let body = client
+        .get(&url)
+        .header("Cookie", cookie)
+        .send()
+        .expect("Failed to send request")
+        .text()
+        .expect("Failed to get response text");
+
+    let file_name = format!("input/day-{}.txt", day);
+    let mut file = File::create(&file_name).expect("Failed to create file");
+
+    file.write_all(body.as_bytes())
+        .expect("Failed to write to file");
+    println!("Wrote input to {}", file_name);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,47 @@
 mod challenges;
 mod day;
+mod fetch_input;
 
 use crate::challenges::day1::Day1;
 use crate::day::Day;
+use crate::fetch_input::fetch_input;
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Which day to run
-    #[arg(short, long)]
-    day: Option<usize>,
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Day {
+        #[arg(short, long)]
+        day: Option<usize>,
+    },
+    FetchInput {
+        #[arg(short, long)]
+        day: usize,
+    },
 }
 
 fn main() {
     let args = Args::parse();
-
     let days: Vec<Box<dyn Day>> = vec![Box::new(Day1)];
 
-    match args.day {
+    match args.command {
+        Commands::Day { day } => day_command(day, days),
+        Commands::FetchInput { day } => fetch_input_command(day, days.len()),
+    }
+}
+
+fn day_command(day: Option<usize>, days: Vec<Box<dyn Day>>) {
+    match day {
         Some(input_day) => {
-            if input_day > days.len() {
-                println!("Day {} not implemented yet", input_day);
-            } else if input_day == 0 || input_day > 25 {
-                println!("Invalid day number: {}", input_day);
-            } else {
+            if validate_day(input_day, days.len()) {
                 let day = &days[input_day - 1];
                 run_day(day, input_day);
             }
@@ -41,4 +57,22 @@ fn main() {
 fn run_day(day: &Box<dyn Day>, day_number: usize) {
     println!("Day {} part 1:\n{}", day_number, day.part1());
     println!("Day {} part 2:\n{}", day_number, day.part2());
+}
+
+fn fetch_input_command(day: usize, days_implemented: usize) {
+    if validate_day(day, days_implemented) {
+        fetch_input(day);
+    }
+}
+
+fn validate_day(day: usize, days_implemented: usize) -> bool {
+    if day == 0 || day > 25 {
+        println!("Invalid day number: {}", day);
+    } else if day > days_implemented {
+        println!("Day {} not implemented yet", day);
+    } else {
+        return true;
+    }
+
+    false
 }
